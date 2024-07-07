cd30b75c2d773dd9637ff9e7f40a424abb733c74
raisaksh1 <raisaksh1>
2024-07-07 19:56:03 +0530

    This is to commit changes

--- app.py
+++ app.py
@@ -3,28 +3,27 @@ from flask import Flask, request, app, jsonify, render_template, url_for
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__)
-regmodel = pickle.load(open('regmodel.py', 'rb'))
+app=Flask(__name__)
+
+#Load the trained model
+regmodel=pickle.load(open('regmodel.pkl', 'rb'))
+scalar=pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/predict_api', methods=['POST'])
-def home():
-    return render_template('home.html')
-
-
 def predict_api():
-    data = request.json['data']
+    data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).rshape(-1, 1))
-    output = regmodel.predict(new_data)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
-
